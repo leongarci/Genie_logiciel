26abb5fa90b67acad0eaf291f7679b327605016b
leongarci <leongarcia1669@gmail.com>
2026-01-26 15:40:38 +0100

    eh beh c'est cadeau

---
+++ classes/src/carte.java
@@ -0,0 +1,12 @@
+public class carte {
+    private String nom;
+    private String type;
+    private String[] themes;
+    private String description;
+    private String localisation;
+    private String ville;
+    private float[] prix;
+    private float[] frequentations;
+    private String description_lieu;
+
+}

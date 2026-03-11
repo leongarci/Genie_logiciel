a9251052cfa1a006b5ad3c000b313f3b905236c2
kevin <kevingrein@gmail.com>
2026-03-11 15:04:24 +0100

    register

--- src/main/java/org/example/Interface/Page/Login.java
+++ src/main/java/org/example/Interface/Page/Login.java
@@ -20,7 +20,7 @@ public class Login extends JPanel {
     private Interface anInterface;
     private BufferedImage back;
     private JLabel err = null;
-
+    private boolean login = true;
     public Login(Interface anInterface, Path pathBackground) {
         super(null);
         this.anInterface = anInterface;
@@ -36,16 +36,39 @@ public class Login extends JPanel {
         JTextField user = textBox(50, 50, 200, 30, "username");
         JTextField pass = textBox(50, 100, 200, 30, "password");
         JButton button = button(50, 150, 200, 30, "Se connecter");
+        JButton sw = button (50, 200, 200, 30, "S’inscrire");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 AuthService authService = new AuthService();
-                int res = authService.login(user.getText(), pass.getText());
+                int res = -1;
+                if (login) {
+                    res = authService.login(user.getText(), pass.getText());
+                }else {
+                    res = authService.register(user.getText(), pass.getText());
+                }
                 if (res != -1) {
                     anInterface.setUser(new User(res, user.getText()));
                     anInterface.show("HOME");
                 }else {
-                    err = texte(50, 200, 220, 30, "Username ou password incorrect", err);
+                    err = texte(50, 250, 220, 30, "Username ou password incorrect", err);
+                    revalidate();
+                    repaint();
+                }
+            }
+        });
+
+        sw.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (login) {
+                    sw.setText("Se connecter");
+                    button.setText("Créé un compte");
+                    login = !login;
+                }else {
+                    sw.setText("S’inscrire");
+                    button.setText("Se connecter");
+                    login = !login;
                 }
             }
         });
@@ -112,7 +135,8 @@ public class Login extends JPanel {
             newtext = texte;
         }else {
             newtext = new JLabel(placeHolder);
-            newtext.setBackground(new Color(.8f, 0, 0, .8f));
+            newtext.setBackground(new Color(.8f, 0, 0, .5f));
+            newtext.setOpaque(true);
             newtext.setBorder(BorderFactory.createCompoundBorder(
                     new FlexibleModernWindow.RoundedPopupBorder(Color.RED, 10),
                     BorderFactory.createEmptyBorder(2, 10, 2, 10)

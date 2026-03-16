971e8895a0a1cba606112b77588ebfa30f5d6715
KTelineSama <jules.holder.54@gmail.com>
2026-03-16 11:18:36 +0100

    add import data pas tout propre mais quasi bon quand même

---
+++ src/main/java/Import.java
@@ -0,0 +1,149 @@
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.CSVWriter;
+
+public class Import {
+
+    private static final String[] MUSEUM_FIELDS = {"identifiant", "nom_officiel", "adresse", "lieu", "code_postal", "ville", "region", "departement", "domaine_thematique", "histoire", "atout", "interet", "annee_creation", "coordonnees"};
+    private static final String[] VISITORS_FIELDS = {"idmuseofile", "annee", "payant", "gratuit", "total", "individuel", "scolaires", "groupes_hors_scolaires", "moins_18_ans_hors_scolaires", "_18_25_ans"};
+    private static final String[] POPULATION_FIELDS = {"dep_l", "newreg_l", "Pop", "trage"};
+    private static final String[] FINAL_FIELDS = Stream.concat(Arrays.stream(MUSEUM_FIELDS), Arrays.stream(VISITORS_FIELDS)).toArray(String[]::new);
+    private static final String MAIN_DATA = "data/musees-de-france-base-museofile.csv";
+    private static final String VISITORS_DATA = "data/ENTREES_ET_CATEGORIES_DE_PUBLIC.csv";
+    private static final String POPULATION_DATA = "data/pop_dep_age_sexe.csv";
+
+    public static void formatGlobal() {
+
+        try {
+            CSVParser parser = new CSVParserBuilder().withSeparator(';').withIgnoreQuotations(true).withStrictQuotes(false)
+                    .withIgnoreLeadingWhiteSpace(true).build();
+
+            //MUSEUMS
+            CSVReader readerMain = new CSVReaderBuilder(new FileReader(MAIN_DATA)).withCSVParser(parser).build();
+            List<String[]> museums = new ArrayList<>();
+            String[] headersMain = readerMain.readNext();
+
+            Map<String, Integer> headerIndexMain = new HashMap<>();
+
+            for (int i = 0; i < headersMain.length; i++) {
+                String normalized = headersMain[i].trim().toLowerCase();
+                if (i == 0) {
+                    normalized = normalized.replace("\uFEFF", "");
+                }
+                normalized = Normalizer.normalize(normalized, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+                headerIndexMain.put(normalized, i);
+            }
+
+            // Filtrage des données pour ne garder que les champs nécessaires
+            String[] lineMain;
+            while ((lineMain = readerMain.readNext()) != null) {
+                if (lineMain.length < headersMain.length) {
+                    System.out.println("Ligne ignorée (nombre de colonnes insuffisant): " + Arrays.toString(lineMain));
+                    continue;
+                }
+                if ("".equals(lineMain[headerIndexMain.get("domaine_thematique")])) {
+                    continue;
+                }
+
+                String[] filteredLine = new String[MUSEUM_FIELDS.length];
+
+                for (int i = 0; i < MUSEUM_FIELDS.length; i++) {
+                    filteredLine[i] = lineMain[headerIndexMain.get(MUSEUM_FIELDS[i])];
+                    if (filteredLine[i] == null) {
+                        filteredLine[i] = "";
+                    }
+                    filteredLine[i] = filteredLine[i].replaceAll("(?<!^)(?<!;)\"(?!;)(?!$)", "");
+                }
+
+                museums.add(filteredLine);
+                //System.out.println("Museum ID: " + filteredLine[0] + ", " + filteredLine[1] + " N°: " + museums.size());
+                //System.out.println("----");
+            }
+            System.out.println("ça sort");
+            readerMain.close();
+
+            //VISITEURS
+            CSVReader readerVisitors = new CSVReaderBuilder(new FileReader(VISITORS_DATA)).withCSVParser(parser).build();
+            List<String[]> visitors = new ArrayList<>();
+            String[] headersVisitors = readerVisitors.readNext();
+
+            Map<String, Integer> headerIndexVisitors = new HashMap<>();
+
+            for (int i = 0; i < headersVisitors.length; i++) {
+                String normalized = headersVisitors[i].trim().toLowerCase();
+                if (i == 0) {
+                    normalized = normalized.replace("\uFEFF", "");
+                }
+                normalized = Normalizer.normalize(normalized, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+                headerIndexVisitors.put(normalized, i);
+            }
+
+            String[] lineVisitors;
+            while ((lineVisitors = readerVisitors.readNext()) != null) {
+                try {
+
+                    if (Integer.parseInt(lineVisitors[headerIndexVisitors.get("total")]) == 0) {
+                        continue;
+                    }
+                    if (Integer.parseInt(lineVisitors[headerIndexVisitors.get("annee")]) != 2023) {
+                        continue;
+                    }
+
+                    String[] filteredLine = new String[VISITORS_FIELDS.length];
+                    for (int i = 0; i < VISITORS_FIELDS.length; i++) {
+                        filteredLine[i] = lineVisitors[headerIndexVisitors.get(VISITORS_FIELDS[i])];
+                        if (filteredLine[i] == null) {
+                            filteredLine[i] = "";
+                        }
+                        filteredLine[i] = filteredLine[i].replaceAll("(?<!^)(?<!;)\"(?!;)(?!$)", "");
+                    }
+                    visitors.add(filteredLine);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+            readerVisitors.close();
+
+            List<String[]> finalMuseums = new ArrayList<>();
+            for (String[] museum : museums) {
+                String id = museum[0];
+                String[] visitorData = visitors.stream().filter(v -> v[0].equals(id)).findFirst().orElse(null);
+                if (visitorData != null) {
+                    finalMuseums.add(Stream.concat(Arrays.stream(museum), Arrays.stream(visitorData)).toArray(String[]::new));
+                } else {
+                    finalMuseums.add(Stream.concat(Arrays.stream(museum), Stream.generate(() -> "").limit(VISITORS_FIELDS.length)).toArray(String[]::new));
+                }
+            }
+
+            // Écriture des données filtrées dans un nouveau fichier CSV
+            CSVWriter writer = new CSVWriter(new FileWriter("data/formatted_museums.csv"));
+            writer.writeNext(FINAL_FIELDS);
+            for (String[] museum : finalMuseums) {
+                writer.writeNext(museum);
+            }
+            writer.close();
+            System.out.println("finito");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+//récupérer themes, premier devient main theme, les autres sont secondaires
+
+//truc avec les départements et régions faut retirer 00- 

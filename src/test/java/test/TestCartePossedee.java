9323fc1e14ccd608d8288decf2ec334734701304
KTelineSama <jules.holder.54@gmail.com>
2026-05-11 17:07:29 +0200

    ok là c'est mieux

--- src/test/java/test/TestCartePossedee.java
+++ src/test/java/test/TestCartePossedee.java
@@ -1,3 +1,7 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 import carte.Carte;
 import carte.CartePossedee;
@@ -5,14 +9,16 @@ import carte.Rarete;
 
 public class TestCartePossedee {
 
+    @Test
     public void testNewCartePossedee() {
         Carte carte = new Carte(1, "Musée du Louvre", "Paris", "Arts", "Histoire", "Atout", "Intérêt", Rarete.COMMUN);
         CartePossedee cartePossedee = new CartePossedee(carte, 3);
 
-        assert cartePossedee.getCarte() == carte;
-        assert cartePossedee.getQuantite() == 3;
+        assertEquals(carte, cartePossedee.getCarte());
+        assertEquals(3, cartePossedee.getQuantite());
     }
 
+    @Test
     public void testSettersEtGetters() {
         Carte carte1 = new Carte(1, "Carte 1", "Paris", "Arts", "Histoire", "Atout", "Intérêt", Rarete.RARE);
         Carte carte2 = new Carte(2, "Carte 2", "Lyon", "Histoire", "Histoire 2", "Atout 2", "Intérêt 2", Rarete.EPIQUE);
@@ -22,7 +28,7 @@ public class TestCartePossedee {
         cartePossedee.setCarte(carte2);
         cartePossedee.setQuantite(5);
 
-        assert cartePossedee.getCarte() == carte2;
-        assert cartePossedee.getQuantite() == 5;
+        assertEquals(carte2, cartePossedee.getCarte());
+        assertEquals(5, cartePossedee.getQuantite());
     }
 }
